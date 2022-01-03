64f5436f58bb188a0090cac264fbca7aaf4d81b4
nifredy <fredydominic0@yahoo.com>
2022-01-03 18:43:11 +0200

    media querries

--- js/app.js
+++ js/app.js
@@ -184,3 +184,69 @@ $(function () {
       },
     });
 });
+
+// // ================ POSTS-IMAGE-MOBILE ===============//
+
+// let sliderWrap = document.querySelector(".slider-wrap");
+// let slider = document.querySelector(".slider");
+// let clonesWidth;
+// let sliderWidth;
+// let clones = [];
+// let disableScroll = false;
+// let scrollPos;
+
+// let items = [...document.querySelectorAll(".slider-item")];
+// let images = [...document.querySelectorAll(".img-div")];
+
+// items.forEach((item) => {
+//   let clone = item.cloneNode(true);
+//   clone.classList.add("clone");
+//   slider.appendChild(clone);
+//   clones.push(clone);
+// });
+
+// function getCloneWidth() {
+//   let width = 0;
+//   clones.forEach((clone) => {
+//     width += clone.offsetWidth;
+//   });
+
+//   return width;
+// }
+
+// function getScrollPos() {
+//   return window.scrollY;
+// }
+
+// function scrollUpdate() {
+//   if (window.innerWidth > 760) {
+//     sliderWrap.style.overflow = 'hidden'
+//     scrollPos = getScrollPos();
+//     if (clonesWidth + scrollPos >= sliderWidth) {
+//       window.scrollTo({ top: 1 });
+//     } else if (scrollPos <= 0) {
+//       window.scrollTo({ top: sliderWidth - clonesWidth - 1 });
+//     }
+
+//     slider.style.transform = `translateX(${-window.scrollY}px)`;
+
+//     requestAnimationFrame(scrollUpdate);
+//   }else{
+//     sliderWrap.style.overflow = 'scroll'
+//   }
+// }
+
+// window.addEventListener('resize', onLoad)
+
+// function onLoad() {
+//   document.body.style.height = `${sliderWidth}px`;
+//   window.scrollTo({ top: 1 });
+//   scrollUpdate();
+// }
+
+// function calculateDimensions() {
+//   sliderWidth = slider.getBoundingClientRect().width;
+//   clonesWidth = getCloneWidth();
+// }
+
+// onLoad();
